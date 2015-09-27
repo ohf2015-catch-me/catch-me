0e888e1cbfa315f56f886d39b10ecc0d0d44f1c6
Philip Frank <ich@philipfrank.de>
2015-09-27 10:19:41 +0200

    fix p2pkit

--- android-client/app/src/main/java/com/example/ashnabhatia/catchme2/P2PManager.java
+++ android-client/app/src/main/java/com/example/ashnabhatia/catchme2/P2PManager.java
@@ -22,7 +22,7 @@ public class P2PManager {
 
     private static final String TAG = "P2PManager";
 
-    private static final String APP_KEY = " eyJzaWduYXR1cmUiOiJyQ0xlN3IwQWdJeWFpTE9rMnlpazd"+
+    private static final String APP_KEY = "eyJzaWduYXR1cmUiOiJyQ0xlN3IwQWdJeWFpTE9rMnlpazd"+
             "0YVJtVzE2YmdvajFEZXE1b1kyV2dVMjYrWGZNLzBmVkhhd1BnTU5pZ1Z0bjVtM2RHdC8rQ1o1UktJc"+
             "jNPbXNlejJjODdSZytsNmVzem1RWS9CUTY5ZDNQMC8ySll2dm1sZ3FQRkxYSFJHQzFjTlBYY1RwWlp"+
             "hcXpQMnpnZGF1M0NCS0YvUTg0SHYxQU1Xdmg0N2JBb2M9IiwiYXBwSWQiOjEyNjMsInZhbGlkVW50a"+
@@ -40,19 +40,18 @@ public class P2PManager {
     private P2PManager(final Context context) {
 
         // fake data
-        games.put(UUID.randomUUID(), "56bf1ae9-7c04-4460-8b02-399ccb57fed0");
+        /*games.put(UUID.randomUUID(), "56bf1ae9-7c04-4460-8b02-399ccb57fed0");
         games.put(UUID.randomUUID(), "f7355cf2-504d-4b66-81d4-872445ca57b8");
         games.put(UUID.randomUUID(), "a44f3d32-0e50-484f-abe2-a26c4b55930f");
         games.put(UUID.randomUUID(), "f54a1d4b-3d15-4a35-8e30-3604f9bbe6a1");
         games.put(UUID.randomUUID(), "dbfa7efc-8827-4a26-8a44-638ea7b80e4b");
-        games.put(UUID.randomUUID(), "7e3ed240-10e4-4d69-90ad-cbc5f6ca369c");
+        games.put(UUID.randomUUID(), "7e3ed240-10e4-4d69-90ad-cbc5f6ca369c");*/
         //games.put(UUID.randomUUID(), "3d2eb515-838c-4905-8602-b6b8b8f8dab7");
 
         final int statusCode = KitClient.isP2PServicesAvailable(context);
         if (statusCode == ConnectionResult.SUCCESS) {
             client = KitClient.getInstance(context);
-            //client.registerConnectionCallbacks(mConnectionCallbacks);
-            client.getDiscoveryServices().addListener(mP2pDiscoveryListener);
+            client.registerConnectionCallbacks(mConnectionCallbacks);
 
             if (client.isConnected()) {
                 Log.d(TAG, "Client already initialized");
@@ -83,6 +82,28 @@ public class P2PManager {
             listener.onGameFound(gameId);
         }
     }
+
+
+    private final ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks() {
+
+        @Override
+        public void onConnected() {
+            Log.d(TAG, "Successfully connected to P2P Services");
+            client.getDiscoveryServices().addListener(mP2pDiscoveryListener);
+
+        }
+
+        @Override
+        public void onConnectionSuspended() {
+            Log.d(TAG, "Connection to P2P Services suspended");
+
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            Log.d(TAG, "Connection to P2P Services failed with status: " + connectionResult.getStatusCode());
+        }
+    };
 
     public void removeListener(GameListener listener) {
         listeners.remove(listener);
@@ -125,9 +146,12 @@ public class P2PManager {
 
         @Override
         public void onPeerDiscovered(final Peer peer) {
-            String info = new String(peer.getDiscoveryInfo());
-            Log.d(TAG, "P2pListener | Peer discovered: " + peer.getNodeId() + " with info: " + info);
-            receivedDiscoveryInfo(peer.getNodeId(), info);
+            byte[] bytes = peer.getDiscoveryInfo();
+            if(bytes != null) {
+                String info = new String(bytes);
+                Log.d(TAG, "P2pListener | Peer discovered: " + peer.getNodeId() + " with info: " + info);
+                receivedDiscoveryInfo(peer.getNodeId(), info);
+            }
         }
 
         @Override
